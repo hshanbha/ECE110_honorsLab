b9f48ae114a5739e5d3c679bdcd475afe494d5a3
Hailan Shanbhag <hailan@eduroamprvnat-172-16-31-91.near.illinois.edu>
2018-04-01 20:43:12 -0500

    updated with buzzer and LCD

--- newtonsLawofCooling110/newtonsLawofCooling110.ino
+++ newtonsLawofCooling110/newtonsLawofCooling110.ino
@@ -1,13 +1,21 @@
 #include <math.h>
+#include <LiquidCrystal.h>
 // ECE 110 Honors Code:
 //
 
 int pin[] = {0,1};
+int time = 0;
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(7,8,9,10,11,12);
+const int buzzer = 9; //buzzer in pin 9
 
 void setup()
 {
   Serial.begin(9600);  //Start the serial connection with the computer
                        //to view the result open the serial monitor 
+  lcd.begin(10,1);    //prep lcd screen
+  lcd.clear();
+  pinMode(buzzer, OUTPUT);
 }
  
 void loop()                     // run over and over again
@@ -20,9 +28,6 @@ void loop()                     // run over and over again
  double voltage[] = {reading[0] * 5.0, reading[1] * 5.0};
  voltage[0] /= 1024.0; 
  voltage[1] /= 1024.0;
- 
- // print out the voltage - for DEBUGGING maybe
- // Serial.print(voltage[0]); Serial.println(" volts");
  
  // calculate temperature in Celsius
  double temperatureC[] = {(voltage[0] - 0.5) * 100, (voltage[1] - 0.5) * 100};  //converting from 10 mv per degree wit 500 mV offset
@@ -40,12 +45,21 @@ void loop()                     // run over and over again
 
  //calculate time left (TO DO: change constant k)
  double timeLeft = NewtonsLawofCooling(temperatureF[0], temperatureF[1], 0.00015, 50);
+
+//if only sample once then print out time left till 0 and exit loop
+ for(int countDown = timeLeft; countDown >= 0; countDown--) {
+   lcd.setCursor(0,0);
+   if(countDown != 0){
+     lcd.print(countDown); lcd.print(" secs left!");
+   } else {
+     lcd.print("All ready!");
+     buzzerPlay();
+   }
+   delay(1000); //wait 1 second
+ }
+ exit(0);
  
- //TO DO:
-        //print out the time left to a console
-        //decide if to continue sampling and updating time how?
- 
- delay(20000);                                     //sample every 20 seconds
+// delay(20000);                                     //sample every 20 seconds
 }
 
 //calculates Newton's Law of Cooling (inverse to find time)
@@ -58,5 +72,16 @@ double NewtonsLawofCooling(double outside, double inside, int k, double final) {
   double inputTolog = (final - outside) / (inside - outside); 
   double time = log(inputTolog) / (double)(-1*k);
   return time;
+}
+
+//buzzer noise function - right now buzzes 3 times
+//Lizzie add music maybe?
+void buzzerPlay() {
+ for(int i = 0; i < 3; i++) { 
+  tone(buzzer, 1000);
+  delay(1000);
+  noTone(buzzer);
+  delay(1000);
+ }
 }
 
